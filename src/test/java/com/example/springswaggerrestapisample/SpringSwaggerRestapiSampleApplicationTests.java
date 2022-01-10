63a844f108182f22b0d08ca72ae5808da8a44d99
branch10480 <toshiharu.imaeda@gmail.com>
2022-01-10 21:50:08 +0900

    first commit

---
+++ src/test/java/com/example/springswaggerrestapisample/SpringSwaggerRestapiSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springswaggerrestapisample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSwaggerRestapiSampleApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}

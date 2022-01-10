87c01a673b6ac5de72efda88746af2eb6945418a
branch10480 <toshiharu.imaeda@gmail.com>
2022-01-10 22:58:32 +0900

    Swagger UI上部の情報の指定

--- src/main/java/com/example/springswaggerrestapisample/SpringSwaggerRestapiSampleApplication.java
+++ src/main/java/com/example/springswaggerrestapisample/SpringSwaggerRestapiSampleApplication.java
@@ -1,7 +1,16 @@
 package com.example.springswaggerrestapisample;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+// この記述で上部のタイトルや説明を指定できる
+@OpenAPIDefinition(info = @Info(
+  title = "Task API",
+  version = "1.0.0",
+  description = "これは、Taskアプリケーションに関するAPIです"
+))
 
 @SpringBootApplication
 public class SpringSwaggerRestapiSampleApplication {
